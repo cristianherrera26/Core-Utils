f958b26b1925d48bb4ff5e402e62c6b7d7d309df
littlefly365 <c081526@hotmail.com>
2026-08-07 17:27:42 -0500

    hostid: simplify command

--- src/hostid/hostid.c
+++ src/hostid/hostid.c
@@ -1,30 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+/*
+ * Copyright (c) 2026, cristianherrera26
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
-static void usage(void);
+#include <stdio.h>
+#include <unistd.h>
 
 int
 main(int argc, char *argv[])
 {
-	int c;
-	while ((c = getopt(argc, argv, "")) != -1) {
-		switch (c) {
-		default:
-			usage();
-		}
-	}
-
-	if (argc != optind)
-		usage();
-
 	printf("%08lx\n", gethostid() & 0xFFFFFFFF);
 	return 0;
-}
-
-static void
-usage(void)
-{
-	fprintf(stderr, "usage: %s\n", getprogname());
-	exit(1);
 }
